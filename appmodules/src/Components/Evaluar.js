f7b771ab5542a6eebcbd108ee84c241d0890f56a
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-03-28 09:09:21 -0500

    React Modulos

---
+++ appmodules/src/Components/Evaluar.js
@@ -0,0 +1,6 @@
+function Evaluar() {
+    let aprendi=true
+    return <h1>{aprendi ? "Aprendiendo React":"No Aprendiendo React"}</h1>
+}
+
+export default Evaluar;
---
+++ appmodules/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {Sumar} from './Components/Sumar'
+import Evaluar from './Components/Evaluar'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<>
+ <Evaluar/>
+</>)
+
