96bf3c8c77c85f41a5fec02f839e503ab5406dd9
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-03-28 10:03:02 -0500

    Estilos

---
+++ app-cards/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Estilos from "./Components/Estilos";
+
+
+function App() {
+  return (
+    <>
+    <Estilos/>
+    </>
+  )
+}
+
+export default App;
---
+++ app-cards/src/Components/Estilos.js
@@ -0,0 +1,13 @@
+const mySuperStyles = {
+    color:"white",
+    fonSize:"14px",
+    border :"1px solid black"
+};
+
+function Estilos() {
+    return (
+        <button style = {mySuperStyles} type="button" className="btn btn-warning">Este es mi Super Botton</button>
+    );
+}
+
+export default Estilos;
