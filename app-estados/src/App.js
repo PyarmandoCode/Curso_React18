1b02dec3bb9cee517d2a0de6849ed5129b89e0cf
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-03-30 09:12:03 -0500

    Propiedades

--- app-estados/src/App.js
+++ app-estados/src/App.js
@@ -1,12 +1,13 @@
 import Propiedades from "./Components/propiedades";
 import Propiedades2 from "./Components/propiedades2";
 import Propiedades3 from "./Components/propiedades3";
+import Propiedades4 from "./Components/propiedades4";
 
 
 function App() {
   return (
     <div >
-      <Propiedades3/>
+      <Propiedades4/>
      
     </div>
   );
