b5e7011f3824f8fd19c436c68cee7212f9a6b246
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-03-30 09:52:30 -0500

    estadoHOOK

--- app-estados/src/App.js
+++ app-estados/src/App.js
@@ -3,12 +3,13 @@ import Propiedades2 from "./Components/propiedades2";
 import Propiedades3 from "./Components/propiedades3";
 import Propiedades4 from "./Components/propiedades4";
 import State1 from "./Components/state1";
+import Example from "./Components/hook1";
 
 
 function App() {
   return (
     <div >
-      <State1/>
+      <Example/>
      
     </div>
   );
