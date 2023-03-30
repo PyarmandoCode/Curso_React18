ae77360ec62d8c04b70bd9d340e4eb3cc4a58034
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-03-30 09:37:07 -0500

    estado

--- app-estados/src/Components/state1.js
+++ app-estados/src/Components/state1.js
@@ -3,13 +3,27 @@ import React from "react";
 class Alumno extends React.Component {
     constructor(props) {
         super(props);
+        //Creacion del Estado Inicial del Objeto
         this.state={nota:"Por Determinar"}
     }
+
+    cambiarNota=() =>{
+        //Modificacion del Estado para alterar alguna parte del componente
+        this.setState({nota:10})
+    }
+
+
     render () {
         return (
             <div>
+                
                 <h1>{this.props.nombre}</h1>
-                <h1>{this.props.nota}</h1>
+                <h1>{this.state.nota}</h1>
+                <button
+                   type="button"
+                   onClick={this.cambiarNota}
+                   >Ver Nota
+                </button>
             </div>
         )
     }
