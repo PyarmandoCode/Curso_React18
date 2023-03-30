031c33392343234797815a3a9e26e754aaa5a1bd
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-03-30 10:00:22 -0500

    Parse

--- app-estados/src/App.js
+++ app-estados/src/App.js
@@ -4,12 +4,13 @@ import Propiedades3 from "./Components/propiedades3";
 import Propiedades4 from "./Components/propiedades4";
 import State1 from "./Components/state1";
 import Example from "./Components/hook1";
+import Sumas from "./Components/cast";
 
 
 function App() {
   return (
     <div >
-      <Example/>
+      <Sumas/>
      
     </div>
   );
