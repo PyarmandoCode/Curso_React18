b581862b6ba3c007bc7748a18bcdb6c038365dd6
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-03-30 08:57:54 -0500

    Anidacion de Componentes

--- app-estados/src/App.js
+++ app-estados/src/App.js
@@ -1,11 +1,12 @@
 import Propiedades from "./Components/propiedades";
 import Propiedades2 from "./Components/propiedades2";
+import Propiedades3 from "./Components/propiedades3";
 
 
 function App() {
   return (
     <div >
-      <Propiedades2/>
+      <Propiedades3/>
      
     </div>
   );
