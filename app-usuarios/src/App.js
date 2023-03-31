381885f7494bd0c2d3f863b8350e20831364f9a7
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-03-31 11:56:26 -0500

    API-USERS

---
+++ app-usuarios/src/App.js
@@ -0,0 +1,12 @@
+import { React , Fragment} from "react";
+import { UserPage } from "./Pages/UserPage";
+
+function  App() {
+  return (
+    <Fragment>
+      <UserPage/>
+    </Fragment>
+  )
+}
+
+export default App;
---
+++ app-usuarios/src/Pages/UserPage.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import { usersApi } from "./api/UserApi";
+
+
+export const UserPage = () => {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    const getUsers = async () => {
+        const resp = await usersApi.get('http://127.0.0.1:8000/api/usuarios/http://127.0.0.1:8000/api/usuarios/');
+        setUsers(resp.data.data);
+
+    }
+
+    //Esto no es HTML es JSX
+    return (
+        <div className="mt-5">
+            <h1>Listado de Usuarios</h1>
+            <hr />
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Email</th>
+                        <th>Name</th>
+                        <th>Avatar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map(user => (
+                            <tr>
+                                <td>{user.id}</td>
+                                <td>{user.email}</td>
+                                <td>{user.first_name} {user.last_name}</td>
+                                <td>
+                                    <img
+                                        src={user.avatar}
+                                        className="img-thumbail"
+                                        style={{
+                                            width: 80
+                                        }}
+                                        alt="" />
+                                </td>
+
+                            </tr>
+
+                        )
+                        )
+                    }
+
+
+                </tbody>
+            </table>
+        </div>
+    )
+
+
+}
