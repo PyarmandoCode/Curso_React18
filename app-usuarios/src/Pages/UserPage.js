d6a11757fe4879e9a251bf1482f040dd640c0614
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-03-31 12:21:36 -0500

    Arquitectura-Final

--- app-usuarios/src/Pages/UserPage.js
+++ app-usuarios/src/Pages/UserPage.js
@@ -1,22 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { usersApi } from "./api/UserApi";
-
+import { UserRow } from "../components/UserRow";
+import { useUsers } from "../hooks/useUsers"
 
 export const UserPage = () => {
+    const { users } = useUsers();
 
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-        getUsers();
-    }, [])
-
-    const getUsers = async () => {
-        const resp = await usersApi.get('https://reqres.in/api/users');
-        setUsers(resp.data.data);
-
-    }
-
-    //Esto no es HTML es JSX
     return (
         <div className="mt-5">
             <h1>Listado de Usuarios</h1>
@@ -33,27 +20,11 @@ export const UserPage = () => {
                 <tbody>
                     {
                         users.map(user => (
-                            <tr>
-                                <td>{user.id}</td>
-                                <td>{user.email}</td>
-                                <td>{user.first_name} {user.last_name}</td>
-                                <td>
-                                    <img
-                                        src={user.avatar}
-                                        className="img-thumbail"
-                                        style={{
-                                            width: 80
-                                        }}
-                                        alt="" />
-                                </td>
-
-                            </tr>
-
-                        )
-                        )
+                            <UserRow
+                                key={user.email}
+                                user={user} />
+                        ))
                     }
-
-
                 </tbody>
             </table>
         </div>
